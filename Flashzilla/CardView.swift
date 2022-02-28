c345a47ff3631e041ec190aa500a3369b06394b6
Sergey Shcheglov <97shcheglov@gmail.com>
2022-02-28 15:35:10 +0600

    Add CardView

--- Flashzilla/CardView.swift
+++ Flashzilla/CardView.swift
@@ -8,13 +8,38 @@
 import SwiftUI
 
 struct CardView: View {
+    let card: Card
+    @State private var isShowingAnswer = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            RoundedRectangle(cornerRadius: 25, style: .continuous)
+                .fill(.white)
+                .shadow(radius: 10)
+            
+            VStack {
+                Text(card.prompt)
+                    .font(.largeTitle)
+                    .foregroundColor(.black)
+                
+                if isShowingAnswer {
+                    Text(card.answer)
+                        .font(.title)
+                        .foregroundColor(.gray)
+                }
+            }
+            .padding()
+            .multilineTextAlignment(.center)
+        }
+        .frame(width: 450, height: 250)
+        .onTapGesture {
+            isShowingAnswer.toggle()
+        }
     }
 }
 
 struct CardView_Previews: PreviewProvider {
     static var previews: some View {
-        CardView()
+        CardView(card: Card.example)
     }
 }
