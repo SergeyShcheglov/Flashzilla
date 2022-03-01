47c1d275596a0cc4d79d8af50a08f951874a60f1
Sergey Shcheglov <97shcheglov@gmail.com>
2022-03-01 13:39:19 +0600

    Add Codable

---
+++ Flashzilla/EditView.swift
@@ -0,0 +1,20 @@
+//
+//  EditView.swift
+//  Flashzilla
+//
+//  Created by Sergey Shcheglov on 01.03.2022.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditView()
+    }
+}
