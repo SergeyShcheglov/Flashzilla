ba40abdcb085306d1ff34ee7abf27350c04fdd91
Sergey Shcheglov <97shcheglov@gmail.com>
2022-03-01 13:40:52 +0600

    Create EditView to add, save and delete cards

--- Flashzilla/EditView.swift
+++ Flashzilla/EditView.swift
@@ -8,8 +8,72 @@
 import SwiftUI
 
 struct EditView: View {
+    @Environment(\.dismiss) var dismiss
+    @State private var cards = [Card]()
+    @State private var newPrompt = ""
+    @State private var newAnswer = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                Section("Add new card") {
+                    TextField("Prompt", text: $newPrompt)
+                    TextField("Answer", text: $newAnswer)
+                    Button("Add card", action: addCard)
+                }
+                
+                Section {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        VStack(alignment: .leading) {
+                            Text(cards[index].prompt)
+                                .font(.headline)
+                            Text(cards[index].answer)
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                    .onDelete(perform: deleteCard)
+                }
+                
+            }
+            .navigationTitle("Edit Cards")
+            
+            .toolbar {
+                Button("Done", action: done)
+            }
+            .listStyle(.grouped)
+            .onAppear(perform: loadData)
+            
+        }
+    }
+    func done() {
+        dismiss()
+    }
+    func loadData() {
+        if let data = UserDefaults.standard.data(forKey: "SavedCards") {
+            if let decoded = try? JSONDecoder().decode([Card].self, from: data) {
+                cards = decoded
+            }
+        }
+    }
+    func save() {
+        if let encoded = try? JSONEncoder().encode(cards) {
+            UserDefaults.standard.set(encoded, forKey: "SavedCards")
+        }
+    }
+    
+    func addCard() {
+        let trimmedPrompt = newPrompt.trimmingCharacters(in: .whitespaces)
+        let trimmedAnswers = newAnswer.trimmingCharacters(in: .whitespaces)
+        guard trimmedPrompt.isEmpty == false && trimmedAnswers.isEmpty == false else { return }
+        
+        let card = Card(prompt: trimmedPrompt, answer: trimmedAnswers)
+        cards.insert(card, at: 0)
+        save()
+    }
+    
+    func deleteCard(at offset: IndexSet) {
+        cards.remove(atOffsets: offset)
+        save()
     }
 }
 
