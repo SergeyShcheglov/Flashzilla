a8fb740fe9fc8c1b0bdfd9cd179a2d28305ee31d
Sergey Shcheglov <97shcheglov@gmail.com>
2022-03-01 12:22:58 +0600

    Add accessibility for DifferentiateWithoutColor

---
+++ Flashzilla/ContentView.swift
@@ -0,0 +1,70 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Sergey Shcheglov on 25.02.2022.
+//
+
+import SwiftUI
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(x: 0, y: offset * 10)
+    }
+}
+struct ContentView: View {
+    @State private var cards = Array<Card>(repeating: Card.example, count: 10)
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    
+    var body: some View {
+        ZStack {
+            Image("background")
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id:\.self) { index in
+                        CardView(card: cards[index]) {
+                            withAnimation {
+                                removeCard(at: index)
+                            }
+                        }
+                            .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
+            if differentiateWithoutColor {
+                VStack {
+                    Spacer()
+                    
+                    HStack {
+                        Image(systemName: "xmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(Circle())
+                        Spacer()
+                        Image(systemName: "checkmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(Circle())
+                    }
+                    .foregroundColor(.white)
+                    .font(.largeTitle)
+                    .padding()
+                }
+            }
+            
+        }
+    }
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
